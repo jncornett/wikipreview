06c99698ad24989ed57f944fadf056958eda42bc
Joel Cornett <joel.cornett@gmail.com>
2016-12-03 14:12:37 -0500

    created basic architecture

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,45 +1,99 @@
 'use strict';
 
+// represents an 'instance' of the extension (tied to a chrome tab)
 class Widget {
   constructor(tabId, enabled) {
     this.tabId = tabId;
-    this.enabled = enabled;
+    this.enabled = enabled == undefined ? true : enabled;
+  }
+  toggle() {
+    if (this.enabled)
+      this.disable();
+    else
+      this.enable();
+    return this.enabled;
+  }
+  enable() {
+    if (!this.enabled) {
+      this.enabled = true;
+      this.notifyContentScript();
+      this.updateExtensionIcon();
+    }
+  }
+  disable() {
+    if (this.enabled) {
+      this.enabled = false;
+      this.notifyContentScript();
+      this.updateExtensionIcon();
+    }
+  }
+  handleRequest(request) {
+    if (request.notify == 'enabled') {
+      this.notifyContentScript();
+      return true;
+    }
+    return false;
+  }
+  notifyContentScript() {
+      chrome.tabs.sendMessage(this.tabId, {enabled: this.enabled});
+  }
+  updateExtensionIcon() {
+    chrome.pageAction.setIcon({
+      tabId: this.tabId,
+      path: this.enabled ?
+        '../images/icon-enabled-128.png' :
+        '../images/icon-disabled-128.png'
+    });
   }
 }
 
-class EnableState {
+// manage the entire app state
+class App {
   constructor() {
-    this.enableMap = {};
+    this.widgetMap = {};
   }
+  getWidget(tabId) {
+    if (!(tabId in this.widgetMap))
+      this.widgetMap[tabId] = new Widget(tabId);
 
-  toggle(tabId) {
-    this.setEnabled(tabId, !this.enabled(tabId));
-    return this.enabled(tabId);
+    return this.widgetMap[tabId];
   }
-
-  enabled(tabId) {
-    const val = this.enableMap[tabId];
-    return val === undefined ? true : val;
-  }
-
-  setEnabled(tabId, val) {
-    this.enableMap[tabId] = val;
+  removeWidget(tabId) {
+    const result = tabId in this.widgetMap;
+    if (result)
+      delete this.widgetMap[tabId];
+    return result;
   }
 }
 
-const enableState = new EnableState();
-
-chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
-});
+const app = new App();
 
 chrome.tabs.onUpdated.addListener(tabId => {
+  console.log('wikipreview - [event] page ' + tabId + ' updated');
   chrome.pageAction.show(tabId);
+  app.getWidget(tabId).updateExtensionIcon();
+});
+
+chrome.tabs.onRemoved.addListener(tabId => {
+  console.log('wikipreview - [event] page ' + tabId + ' closed');
+  const result = app.removeWidget(tabId);
+  if (!result)
+    throw 'wikipreview - could not remove widget for ' + tabId +
+      ' because it does not exist';
+});
+
+chrome.runtime.onMessage.addListener((request, sender) => {
+  console.log('wikipreview - [event] receive message from ' + sender.tab.id +
+      ': ' + JSON.stringify(request));
+  const result = app.getWidget(sender.tab.id).handleRequest(request);
+  if (!result)
+    throw 'wikipreview - could not handle message from ' + sender.tab.id;
 });
 
 chrome.pageAction.onClicked.addListener(tab => {
-  chrome.pageAction.setTitle({title: 'click' + new Date(), tabId: tab.id});
-  console.log('page action clicked');
+  console.log('wikipreview - [event] page action clicked for ' + tab.id);
+  const result = app.getWidget(tab.id).toggle();
+  console.log('wikpreview - enabled is ' + result + ' for ' + tab.id);
 });
 
 console.log('wikipreview - background.js loaded');
--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -1,3 +1,59 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+class Widget {
+  constructor() {
+    this.initialized = false;
+    this.enabled = false;
+  }
+  log() {
+    let args = [...arguments];
+    args.unshift("wikipreview -");
+    console.log(...args);
+  }
+  initialize() {
+    if (this.initialized)
+      return;
+    this.log('intializing');
+    this.initialized = true;
+  }
+  enable() {
+    if (this.enabled)
+      return;
+    this.initialize();
+    this.log('enabling');
+    this.enabled = true;
+  }
+  disable() {
+    if (!this.enabled)
+      return;
+    this.log('disabling');
+    this.enabled = false;
+  }
+  handleMessage(message) {
+    this.log('handle message:', JSON.stringify(message));
+    if ('enabled' in message) {
+      if (message.enabled)
+        this.enable();
+      else
+        this.disable();
+      return true;
+    }
+    return false;
+  }
+}
+
+// entry point
+$(function() {
+  const widget = new Widget();
+  // add a listener for enable/disable requests
+  chrome.runtime.onMessage.addListener(message => {
+    const result = widget.handleMessage(message);
+    if (!result)
+      throw 'could not handle message from background script';
+  });
+
+  // let the background script know we're ready to receive enable/disable requests
+  // this will trigger the initial state message
+  console.log('wikipreview - request initial enabled state');
+  chrome.runtime.sendMessage({notify: 'enabled'});
+});
