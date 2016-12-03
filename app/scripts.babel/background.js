d77dd743dddc8b44c9575a6e6bb29e20a72e61dd
Joel Cornett <joel.cornett@gmail.com>
2016-12-03 11:48:19 -0500

    some icon and pageaction click experimentation

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,5 +1,34 @@
 'use strict';
 
+class Widget {
+  constructor(tabId, enabled) {
+    this.tabId = tabId;
+    this.enabled = enabled;
+  }
+}
+
+class EnableState {
+  constructor() {
+    this.enableMap = {};
+  }
+
+  toggle(tabId) {
+    this.setEnabled(tabId, !this.enabled(tabId));
+    return this.enabled(tabId);
+  }
+
+  enabled(tabId) {
+    const val = this.enableMap[tabId];
+    return val === undefined ? true : val;
+  }
+
+  setEnabled(tabId, val) {
+    this.enableMap[tabId] = val;
+  }
+}
+
+const enableState = new EnableState();
+
 chrome.runtime.onInstalled.addListener(details => {
   console.log('previousVersion', details.previousVersion);
 });
@@ -8,4 +37,9 @@ chrome.tabs.onUpdated.addListener(tabId => {
   chrome.pageAction.show(tabId);
 });
 
-console.log('\'Allo \'Allo! Event Page for Page Action');
+chrome.pageAction.onClicked.addListener(tab => {
+  chrome.pageAction.setTitle({title: 'click' + new Date(), tabId: tab.id});
+  console.log('page action clicked');
+});
+
+console.log('wikipreview - background.js loaded');
